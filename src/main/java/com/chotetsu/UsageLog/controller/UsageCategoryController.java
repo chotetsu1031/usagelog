30385e1404f2212ea78711ee0c96fe21982d3b1e
Tetsu <tetsu994@icloud.com>
2025-12-21 12:32:17 +0900

    add category edit page

--- src/main/java/com/chotetsu/UsageLog/controller/UsageCategoryController.java
+++ src/main/java/com/chotetsu/UsageLog/controller/UsageCategoryController.java
@@ -1,10 +1,12 @@
 package com.chotetsu.UsageLog.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class UsageCategoryController {
   @GetMapping("/edit")
-  public String editCategory() {
+  public String showEditPage() {
     // edit.htmlを開く
     return "edit";
   }
